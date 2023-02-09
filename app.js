2797637db26c67b1ff9066597628271154b00d7f
Gary White <garywhite61@hotmail.com>
2023-02-09 13:22:24 +0100

    code

---
+++ app.js
@@ -0,0 +1,7 @@
+function joinStrings(string1, string2){
+    // code here
+}
+
+console.log(joinStrings('string1', 'string2')); // 'string1 string2'
+console.log(joinStrings('testing', 'testing')); // 'testing testing'
+console.log(joinStrings('134, 234')); // '134 234'
